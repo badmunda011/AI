c999c034e6affc78b3638e1e60df7d13467172a5
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-08-09 11:00:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-API_ID :int = int(os.environ.get("API_ID", None))
-API_HASH :str = os.environ.get("API_HASH", "")
-BOT_TOKEN :str= os.environ.get("BOT_TOKEN", "")
+API_ID :int = int(os.environ.get("API_ID", 25742938))
+API_HASH :str = os.environ.get("API_HASH", "b35b715fe8dc0a58e8048988286fc5b6")
+BOT_TOKEN :str= os.environ.get("BOT_TOKEN", "7201253389:AAG4qdCWAFprgCafSK5fwLC0zQrfvRts0gA")
 UPDATE_CHNL :str = os.environ.get("UPDATE_CHNL", "mr_sukkun")
 SUPPORT_GRP :str = os.environ.get("SUPPORT_GRP", "the_support_chat")
 START_IMG :str = os.environ.get(
@@ -10,7 +10,7 @@ START_IMG :str = os.environ.get(
 
 MONGO_DB_URI :str = os.environ.get(
     "MONGO_DB_URI",
-    "",
+    "mongodb+srv://Badmunda_13:badmunda50@cluster0.9oyzqux.mongodb.net/?retryWrites=true&w=majority",
 )
 OWNER_ID :int= os.environ.get("OWNER_ID", 6728038801)
 
